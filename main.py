666cc4282a7baffb50d2514879648f73ef882cec
BintangWU <bpratama348@gmail.com>
2024-08-03 00:32:55 +0700

    add preview cropped img

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import json, time, requests
 import cv2 as cv
 import numpy as np
 from pydantic import BaseModel
+from typing import Optional
 
 
 class RegionMarking(BaseModel):
     region: int
+    part: Optional[str] = ''
     position: list
 
 
@@ -21,6 +23,7 @@ x_start, y_start, x_end, y_end = 0, 0, 0, 0
 
 
 def mouse_marking(envent, x, y, flags, param):
+    global img
     global mark_action
     global region_cnt
     global region_cnt
@@ -40,6 +43,9 @@ def mouse_marking(envent, x, y, flags, param):
 
         region_cnt += 1
         region_save.append(RegionMarking(region= region_cnt, position= [ x_start, y_start, x_end, y_end]).model_dump())
+        
+        crop_img = img[y_start:y_end, x_start:x_end]
+        cv.imshow("Cropped", crop_img)
 
 
 def save_region_file():
@@ -65,6 +71,7 @@ def laod_region_file(file_name: str):
 
 
 def main(source: str, interval: int):
+    global img
     global mark_action
     global region_cnt
     global region_cnt
@@ -72,8 +79,7 @@ def main(source: str, interval: int):
 
     counter = 0
     mode = 0
-    last_time = 0
-    start_time = time.time()
+    last_time = time.time()
 
     defauilt_source = f"http://{source}/ISAPI/Streaming/channels/101/picture"
     cv.namedWindow(default_window_name)
@@ -81,7 +87,7 @@ def main(source: str, interval: int):
         named_tuple = time.localtime()
         time_string = time.strftime("%m%d%Y", named_tuple)
 
-        response = requests.get(defauilt_source, auth=('admin', 'Hik@123.!'), stream=True).raw
+        response = requests.get(defauilt_source, auth=('admin', 'Changeyourpwd366'), stream=True).raw
         img_raw = np.asarray(bytearray(response.read()), dtype='uint8')
         img_raw = cv.imdecode(img_raw, cv.IMREAD_COLOR)
 
@@ -96,14 +102,14 @@ def main(source: str, interval: int):
         
         elif mode == 2: # Collect dataset
             # cv.putText(img_scale, f"Mode: {label[mode]}", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2, cv.LINE_AA)
-            if last_time > interval:
+            if (time.time() - last_time) > interval:
                 for region in region_save:
                     counter += 1
                     img_crop = img[region["position"][1]:region["position"][3], region["position"][0]:region["position"][2]]
                     cv.imwrite(f"./dataset/IMG{counter}_{source}_{time_string}.jpeg", img_crop)
                     time.sleep(0.1)
 
-                start_time = time.time()
+                last_time = time.time()
         
         elif mode == 0: # Normal mode
             mark_action = False
@@ -118,11 +124,10 @@ def main(source: str, interval: int):
         if mark_action:
             cv.rectangle(img_scale, (x_start, y_start), (x_end, y_end), (0, 0, 255), 1)
 
-        end_time = time.time()
-        last_time = end_time - start_time
+        
             
         cv.imshow(default_window_name, img_scale)
-        key = cv.waitKey(50)
+        key = cv.waitKey(5)
         if key == ord('q'): # Quit windows
             break
 
@@ -155,4 +160,4 @@ if __name__ == "__main__":
     interval = input("Interval Collection: ")
 
     main(source, int(interval))
-    print("OK")
+    print("APP CLOSE!")
